1438dec51acb14c27a137670a47b3aa3dbc08d3e
anuragkurosaki <60319638+anuragkurosaki@users.noreply.github.com>
2020-05-07 20:10:23 +0530

    Update MainActivity.java

--- app/src/main/java/com/anurag/cafeteriabookings/MainActivity.java
+++ app/src/main/java/com/anurag/cafeteriabookings/MainActivity.java
@@ -41,13 +41,13 @@ public class MainActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d("loginPressed", "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
+//
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w("loginPressed", "signInWithEmail:failure", task.getException());
-                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-                            updateUI(null);
+//
                             // ...
                         }
 
