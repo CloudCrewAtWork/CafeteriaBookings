0b9d95728c7458f8c598f96035eeecba7e8573b5
anuragkurosaki <60319638+anuragkurosaki@users.noreply.github.com>
2020-05-07 18:35:27 +0530

    Added the firebase code

--- app/src/main/java/com/anurag/cafeteriabookings/MainActivity.java
+++ app/src/main/java/com/anurag/cafeteriabookings/MainActivity.java
@@ -1,29 +1,69 @@
 package com.anurag.cafeteriabookings;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
-    EditText email;
-    EditText password;
+    EditText loginId;
+    EditText passCode;
     Button register;
     Button login;
+
+    public void loginPressed(View view){
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                            // ...
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        email = findViewById(R.id.EmaileditText);
-        password = findViewById(R.id.PasswordeditText);
+        loginId = findViewById(R.id.EmaileditText);
+        passCode = findViewById(R.id.PasswordeditText);
         register = findViewById(R.id.Registerbutton);
         login = findViewById(R.id.Loginbutton);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
 
     }
+
 }
